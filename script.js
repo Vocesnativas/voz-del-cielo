e38107044ce20d2207d338bd2aecc96188a9eb40
Vocesnativas <yhonysrafael1970@gmail.com>
2026-08-12 14:19:32 -0500

    Add button existence check in script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const boton = document.getElementById("nuevo-versiculo");
 
+    if (!boton) {
+        alert("NO ENCUENTRO EL BOTÓN");
+        return;
+    }
+
     boton.addEventListener("click", function () {
         alert("¡EL BOTÓN FUNCIONA!");
     });
